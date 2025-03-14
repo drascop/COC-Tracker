98299a4a6b3dac6c049ab7ff0049968336038095
Dan Rascop <80607356+drascop@users.noreply.github.com>
2025-03-14 08:12:19 -0500

    Initialize Spring Boot App

---
+++ coc-tracker/src/main/java/com/drascop/coctracker/CocTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.drascop.coctracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocTrackerApplication.class, args);
+	}
+
+}
---
+++ coc-tracker/src/test/java/com/drascop/coctracker/CocTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drascop.coctracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
