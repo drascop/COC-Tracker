607aa2dd17891b1900f3107bfa50c5a9c7f25ca2
Dan Rascop <80607356+drascop@users.noreply.github.com>
2025-03-21 13:16:52 -0500

    dockerfile changes, change main method file name

--- backend/src/main/java/com/drascop/coctracker/Application.java
+++ backend/src/main/java/com/drascop/coctracker/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CocTrackerApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CocTrackerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
